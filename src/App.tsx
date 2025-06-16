e92414f1e159aae4f5034d4c3018e34c442cca4b
Wyhang <72087988+Tanwyhang@users.noreply.github.com>
2025-06-16 15:05:57 +0800

    Remove Footer from All Pages Except Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { Navbar } from './components/Layout/Navbar';
 import { Footer } from './components/Layout/Footer';
@@ -13,27 +13,36 @@ import { ModelDetailPage } from './pages/ModelDetailPage';
 import { SignInForm } from './components/Auth/SignInForm';
 import { SignUpForm } from './components/Auth/SignUpForm';
 
+function AppContent() {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <div className="min-h-screen flex flex-col relative">
+      <InteractiveDots />
+      <Navbar />
+      <main className="flex-1 relative z-10">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/marketplace" element={<MarketplacePage />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/test" element={<TestModelPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/models/:id" element={<ModelDetailPage />} />
+          <Route path="/signin" element={<SignInForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+        </Routes>
+      </main>
+      {isHomePage && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="min-h-screen flex flex-col relative">
-          <InteractiveDots />
-          <Navbar />
-          <main className="flex-1 relative z-10">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/marketplace" element={<MarketplacePage />} />
-              <Route path="/upload" element={<UploadPage />} />
-              <Route path="/test" element={<TestModelPage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/models/:id" element={<ModelDetailPage />} />
-              <Route path="/signin" element={<SignInForm />} />
-              <Route path="/signup" element={<SignUpForm />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <AppContent />
       </Router>
     </AuthProvider>
   );
